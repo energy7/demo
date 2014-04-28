5306d27eb2e26c79083ffb5c24d73cb074a80c7a
Erica Heinz <e@energy7.com>
2014-04-28 00:04:40 -0400

    Style retreat page

--- demosite/assets/js/app.js
+++ demosite/assets/js/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   
-  // Write your Javascript!
+    // http://codepen.io/bradfrost/pen/sHvaz
+    
+    $('body').addClass('js');
+    var $menu = $('#menu'),
+    $menulink = $('.menu-link');
+    
+    $menulink.click(function() {
+        $menulink.toggleClass('active');
+        $menu.toggleClass('active');
+        return false;
+    });
+
 
 });
